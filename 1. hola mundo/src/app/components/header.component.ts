95afb20112214e4e895ab91124da7daf16f86426
ofmartinezm <oscar.masmela@gmail.com>
2018-01-11 11:25:49 -0500

    adicion carpeta primer proyecto angular

---
+++ 1. hola mundo/src/app/components/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-header',
+  template: 
+})
+export class AppComponent {
+  nombre = 'Oscar';
+  apellido = 'Martinez';
+}
